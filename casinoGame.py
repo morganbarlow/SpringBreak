f48cd93b1e8639c1fe14f55317028c55376f9410
morganbarlow <morgan.barlow2232@gmail.com>
2021-03-16 22:25:08 -0600

    A couple more functionalities

--- casinoGame.py
+++ casinoGame.py
@@ -4,17 +4,27 @@ from numpy import random
 list_of_nos = ["no","no thank you", "I'm alright", "I'll pass", "nah", "nope", "not gonna happen", "never", "pass", "not today", "I can't", "no way", "not a chance", "I'd rather not"]
 
 def userGuess (wallet, houseCard) :
-  print(houseCard)
-  print(wallet)
+  # print(houseCard)
+  print(f'${wallet} left in our wallet.')
   user_guess = input("What will your guess be?\n")
+
+  def continueGame(wallet, question) :
+    letsPlay = input(question)
+    
+    if letsPlay in list_of_nos :
+      print("Well have a good day.")
+    else:  
+      placeBet(wallet)
+
   if user_guess in houseCard.lower() :
     print("Congrats!")
     user_wallet = wallet + 10
     print (f'You now have ${user_wallet} to play with.')
-    letsPlayAGame('Would you like to go another round?')
+    continueGame(user_wallet,'Would you like to go another round?')
   else: 
     print("Not a winner this round.")
-    letsPlayAGame("Would you like to try again?\n")
+    print(user_wallet)
+    continueGame(user_wallet, "Would you like to try again?\n")
 
 def ourGame(bet,wallet) :
   guessByColor = {
@@ -48,6 +58,7 @@ def ourGame(bet,wallet) :
   if bet == '5' :
     house_card = random.choice(list(guessByColor.keys()))
     user_wallet = wallet - 5
+    print(house_card)
     userGuess(user_wallet,house_card)
   elif bet == '3' :
     house_card = random.choice(list(guessBySuit.keys()))
@@ -63,7 +74,7 @@ def ourGame(bet,wallet) :
 
 
 def placeBet (user_wallet) :
-  user_bet = input ("How much would you like to bet?\n It is ...\n $5 to guess the color.\n $3 to guess the suit.\n $1 to guess the number or face.\n")
+  user_bet = input ("How much would you like to bet?\n It is ...\n $5 to guess the card color.\n $3 to guess the card suit.\n $1 to guess the card number or face.\n")
 
   ourGame(user_bet,user_wallet)
   
@@ -79,7 +90,7 @@ def letsPlayAGame (question) :
   letsPlay = input(question).lower()
   if letsPlay in list_of_nos : {
     print("Well that is too bad.")
-  } 
+  }
   else : {
     getWallet(input("what's your wallet limit?\n"))
   }
